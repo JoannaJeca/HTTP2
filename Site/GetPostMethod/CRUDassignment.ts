ca55433d1f2c97b2f03d7de9b8f1aba14e0fc5b6
JoanJeca <joannajeca936@gmail.com>
2023-09-25 01:23:55 +0100

    Fifth commiton Update(CRUD)

--- Site/GetPostMethod/CRUDassignment.ts
+++ Site/GetPostMethod/CRUDassignment.ts
@@ -13,28 +13,28 @@ interface iMessage{
     data:null | {} | {}[]
 }
 
-let Data:iData[] =[
+let UserInfo:iData[] =[
     {
         id:1,
-        name:"Jemima",
+        name:"Ayo",
         class:"Set08",
         age:25
     },
     {
         id:2,
-        name:"Jessica",
+        name:"Tobi",
         class:"Set08",
         age:27
     },
     {
         id:3,
-        name:"Regina",
+        name:"Habeeb",
         class:"Set08",
         age:30
     },
     {
         id:4,
-        name:"Joan",
+        name:"Wisdom",
         class:"Set08",
         age:35
     },
@@ -42,126 +42,104 @@ let Data:iData[] =[
 ] 
 
 const port = 2008
-const server = http.createServer((req:IncomingMessage, res:ServerResponse<IncomingMessage>)=>{
+const mYserver = http.createServer((req:IncomingMessage, res:ServerResponse<IncomingMessage>)=>{
     res.setHeader("Content-type", "application/json")
-    const {method, url} = req
+    const {url, method} = req
 
     let status = 404
-    let message:iMessage = {
-        message:"Unsuccessful",
+    const response:iMessage={
+        message:"Failed",
         success:false,
         data:null
     }
 
     const container:any = []
 
-    req 
+    req
        .on("data", (chunk:any)=>{
         container.push(chunk)
        })
        .on("end", ()=>{
-        //get method 
-        if(method ==="GET" && url === "/"){
-            status = 200
-            message.message="Successfully read"
-            message.success=true,
-            message.data=Data,
-            res.write(JSON.stringify({message, status}))
-            res.end()
-        }
-        //POST method
-        if(url === "/" && method ==="POST"){
-            const body= JSON.parse(container)
-            Data.push(body)
-            status = 201
-            message.message="SUccessfully created"
-            message.success=true,
-            message.data=Data
-            res.write(JSON.stringify({status, message}))
-            res.end()
-        }
         //PATCH method
-        if(method ==="PATCH"){
+        if(method === "PATCH"){
             const build = JSON.parse(container)
-            let getId:any = url?.split("/")[1]
-            let parsing = parseInt(getId)
-            
-            let findId = Data.some((el:any)=>{
-                return el.id === parsing
-            })
+            const details:any = url?.split('/')[1]
+            const dataValue = parseInt(details)
 
-            if(findId === false){
-                status = 404
-                message.message="Unsuccessful"
-                message.success=false
-                message.data=null,
-                res.write(JSON.stringify({status, message}))
+            let findObject = UserInfo.some((el:any)=>{
+                return el.id === dataValue
+            })
+            if(findObject === false){
+                status = 404,
+                response.message="Unsuccessful in updating"
+                response.success=false,
+                response.data=null,
+                res.write(JSON.stringify({response, status}))
                 res.end()
             }else{
-                const updateName = build.name
-                Data = Data.map((user:any)=>{
-                if(user?.id === parsing){
-                    return{
-                        id:user?.id,
-                        name: updateName,
-                        class:user?.class,
-                        age:user?.age
-                    }
-                }return user
-                    
+                const updateAge = build.age
+                UserInfo = UserInfo.map((user:any)=>{
+                    if(user?.id === dataValue){
+                        return{
+                            id: user?.id,
+                            name: user?.name,
+                            class:user?.class,
+                            age:updateAge
+                        }
+                    }return user
                 })
-
-                status = 200
-                message.message="Successfully changed"
-                message.success=true
-                message.data=Data
-                res.write(JSON.stringify({message, status}))
+                status = 200,
+                response.message="Successful in updating"
+                response.success=true,
+                response.data=UserInfo,
+                res.write(JSON.stringify({response, status}))
                 res.end()
             }
         }
         //PUT method
         if(method === "PUT"){
             const build = JSON.parse(container)
-            const details:any = url?.split("/")[1]
-            const parsedDetails = parseInt(details)
+            const details:any = url?.split('/')[1]
+            const dataValue = parseInt(details)
 
-            let findId = Data.some((el:any)=>{
-                return el.id === parsedDetails
+            let findObject = UserInfo.some((el:any)=>{
+                return el.id === dataValue
             })
-            if(findId === false){
-                status = 404
-                message.message="Failed in changing"
-                message.success=false,
-                message.data=null,
-                res.write(JSON.stringify({message, status}))
+            if(findObject === false){
+                status = 404,
+                response.message="Unsuccessful in updating"
+                response.success=false,
+                response.data=null,
+                res.write(JSON.stringify({response, status}))
                 res.end()
             }else{
                 const updateAge = build.age
+                const updateName = build.name
                 const updateClass = build.class
 
-                Data = Data.map((user:any)=>{
-                    if(user?.id === parsedDetails){
+                UserInfo = UserInfo.map((user:any)=>{
+                    if(user?.id === dataValue){
                         return{
                             id: user?.id,
-                            name:user?.name,
-                            class: updateClass,
+                            name: updateName,
+                            class:updateClass,
                             age:updateAge
                         }
-                    }
-                    return user;
+                    }return user
                 })
-                status = 200
-                message.message="Succesfully in global changing"
-                message.success=true,
-                message.data=Data,
-                res.write(JSON.stringify({message, status}))
+                status = 200,
+                response.message="Successful in updating global"
+                response.success=true,
+                response.data=UserInfo,
+                res.write(JSON.stringify({response, status}))
                 res.end()
             }
-        } 
-        
-       })
-})
+        }
 
-server.listen(port, ()=>{
+       })
+ })
+
+
+ mYserver.listen("port", ()=>{
     console.log("Port running on", port)
-})
+ })
